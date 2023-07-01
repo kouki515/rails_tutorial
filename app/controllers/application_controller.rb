a02f108cf6f55c89a5d38286eabc9ada296533d6
kouki515 <71614240+kouki515@users.noreply.github.com>
2023-07-02 08:24:55 +0900

    adds hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    def hello
+        render html: "hello world!"
+    end
+
+    def goodbye
+        render html: "good bye"
+    end
 end
